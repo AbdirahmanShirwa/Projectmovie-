9d2f8c3bd7b12c08035e8e272bb22a8405eeee5a
AbdirahmanShirwa <78727561+AbdirahmanShirwa@users.noreply.github.com>
2022-04-24 16:56:18 +0300

    ok

--- main.js
+++ main.js
@@ -1,47 +1,86 @@
+const inputField = document.querySelector('.movie__search');
+const boxes = document.querySelector('main');
+let aboutUs = document.querySelector('.about__movie');
 
-const searchBtn = document.getElementById('search-btn');
-const mealList = document.getElementById('meal');
-const mealDetailsContent = document.querySelector('.meal-details-content');
-const recipeCloseBtn = document.getElementById('recipe-close-btn');
-
-// event listeners
-searchBtn.addEventListener('click', getMealList);
-
-recipeCloseBtn.addEventListener('click', () => {
-    mealDetailsContent.parentElement.classList.remove('showRecipe');
+inputField.addEventListener('keyup', (e) => {
+    if(e.keyCode === 13) {
+        const searchMovie = inputField.value;
+        boxes.innerHTML = '';
+        loadMovies(searchMovie);
+    }
+    e.preventDefault();
 });
 
+const loadMovies = (searchMovie) => {
+    fetch(`https://www.omdbapi.com/?s=${searchMovie}&apikey=19ec95fc`)
+    .then(response => 
+        {
+            if(!response.ok)
+                throw new Error(`Status Code Error: ${response.status}`);
+            response.json()
+            .then((data => {
+                    showMovies(data.Search);
+            }));
+        })
+        .catch((err) => {
+            console.log('Something wrong!');
+            console.log(err);
+        })
+}
+const showMovies = (movies) => {
+    if(movies === undefined)
+    {
+        alert('Movie doesnt exist');
+    } else {
+        movies.forEach((movie) => {
+            const {Title, Year, Poster, imdbID} = movie;
+            const movieBox = document.createElement('div');
+            movieBox.classList.add('box');
+            boxes.appendChild(movieBox);
 
-// get meal list that matches with the ingredients
-function getMealList(){
-    let searchInputTxt = document.getElementById('search-input').value.trim();
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
-    .then(response => response.json())
-    .then(data => {
-        let html = "";
-        if(data.meals){
-            data.meals.forEach(meal => {
-                html += `
-                    <div class = "meal-item" data-id = "${meal.idMeal}">
-                        <div class = "meal-img">
-                            <img src = "${meal.strMealThumb}" alt = "food">
-                        </div>
-                        <div class = "meal-name">
-                            <h3>${meal.strMeal}</h3>
-                            <a href = "#" class = "recipe-btn">Get Recipe</a>
-                        </div>
-                    </div>
+            let imgPoster = Poster === 'N/A' ? 'images/na.jpeg' : Poster;
+            movieBox.innerHTML = `
+            <img src="${imgPoster}" width="100%"/ alt="${Title}" onClick="movieSelected('${imdbID}')">
+                <p>${Title} (${Year})</p>
                 `;
-            });
-            mealList.classList.remove('notFound');
-        } else{
-            html = "Sorry, we didn't find any meal!";
-            mealList.classList.add('notFound');
-        }
+        });
+    }
+}
+const movieSelected = (imdbID) => {
+    fetch(`https://www.omdbapi.com/?i=${imdbID}&apikey=19ec95fc`)
+    .then(response => 
+        {
+            if(!response.ok)
+                throw new Error(`Status Code Error: ${response.status}`);
+            response.json()
+            .then((data => {
+                    let { Poster, Title, Released, BoxOffice, Genre, imdbRating, Actors } = data;
+                    aboutUs.style.display = 'block';
 
-        mealList.innerHTML = html;
-    });
+
+                    let card = document.createElement('div');
+                    let btn = document.createElement('button');
+
+                    card.classList.add('about__movie__card');
+                    aboutUs.appendChild(card);
+                
+                    document.querySelector('#Poster').src = ` ${Poster}`;
+                    document.querySelector('#movie_name').innerHTML = ` ${Title}`;
+                    document.querySelector('#movie_released').innerHTML = ` ${Released}`;
+                    document.querySelector('#movie_boxOffice').innerHTML = ` ${BoxOffice}`;
+                    document.querySelector('#movie_imdb').innerHTML = ` ${imdbRating}`;
+                    document.querySelector('#movie_genre').innerHTML = ` ${Genre}`;
+                    document.querySelector('#movie_actors').innerHTML = ` ${Actors}`;
+
+            }));
+        })
+        .catch((err) => {
+            console.log('Something wrong!');
+            console.log(err);
+        })
+    }
+const closeWindow = () => {
+        aboutUs.style.display = 'none';
+        const card = document.querySelector('.card');
 }
 
-
-// get recipe of the meal
