e685a085048ad52940e7be2d0bca63074d25a0d4
AbdirahmanShirwa <78727561+AbdirahmanShirwa@users.noreply.github.com>
2022-04-24 18:16:27 +0300

    ok

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-//  Here we  modify the values of the arrays
 const inputField = document.querySelector('.movie__search');
 const boxes = document.querySelector('main');
 let aboutUs = document.querySelector('.about__movie');
 
-
-// We added enter key for searching movies
 inputField.addEventListener('keyup', (e) => {
     if(e.keyCode === 13) {
         const searchMovie = inputField.value;
@@ -13,8 +10,6 @@ inputField.addEventListener('keyup', (e) => {
     }
     e.preventDefault();
 });
-
-// If the search value is found in the Api it will show the movie.Otherwise i will alert No movie found
 
 const loadMovies = (searchMovie) => {
     fetch(`https://www.omdbapi.com/?s=${searchMovie}&apikey=19ec95fc`)
@@ -28,14 +23,14 @@ const loadMovies = (searchMovie) => {
             }));
         })
         .catch((err) => {
-            console.log('Opps something went Wrong !');
+            console.log('Something wrong!');
             console.log(err);
         })
 }
 const showMovies = (movies) => {
     if(movies === undefined)
     {
-        alert('No movies found');
+        alert('Movie doesnt exist');
     } else {
         movies.forEach((movie) => {
             const {Title, Year, Poster, imdbID} = movie;
@@ -43,7 +38,7 @@ const showMovies = (movies) => {
             movieBox.classList.add('box');
             boxes.appendChild(movieBox);
 
-           
+            let imgPoster = Poster === 'N/A' ? 'images/na.jpeg' : Poster;
             movieBox.innerHTML = `
             <img src="${imgPoster}" width="100%"/ alt="${Title}" onClick="movieSelected('${imdbID}')">
                 <p>${Title} (${Year})</p>
@@ -51,7 +46,6 @@ const showMovies = (movies) => {
         });
     }
 }
-// If the user clicks the Movie it will show the information about the movie
 const movieSelected = (imdbID) => {
     fetch(`https://www.omdbapi.com/?i=${imdbID}&apikey=19ec95fc`)
     .then(response => 
@@ -85,7 +79,6 @@ const movieSelected = (imdbID) => {
             console.log(err);
         })
     }
-    // Here the user can close the window about detailed information of the movie
 const closeWindow = () => {
         aboutUs.style.display = 'none';
         const card = document.querySelector('.card');
